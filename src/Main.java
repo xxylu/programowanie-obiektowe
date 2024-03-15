6b41eca516d73a76b7f9a451d27d55103329fe9c
xxylu <80449534+xxylu@users.noreply.github.com>
2024-03-15 12:08:33 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ public class Main {
         Point p1 = new Point(120,40);
         Point p2 = new Point(10,76);
 
-        Segment s = new Segment();
-        s.setP1(p1);
-        s.setP2(p2);
+        Segment s = new Segment(p1,p2);
         System.out.println(s.distance());
     }
 }
--- src/Segment.java
+++ src/Segment.java
@@ -1,15 +1,17 @@
 public class Segment{
-    private Point p1,p2;
-    public Point getP1() {
-        return p1;
-    }
-    public Point getP2() {
-        return p2;
-    }
+    private Point p1;
+    private Point p2;
+    /*
     public void setP1(Point p1) {
         this.p1 = p1;
     }
     public void setP2(Point p2) {
+        this.p2 = p2;
+    }
+    */
+
+    Segment(Point p1, Point p2){
+        this.p1 = p1;
         this.p2 = p2;
     }
 
@@ -17,4 +19,16 @@ public class Segment{
         double distance = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
         return distance;
     }
+
+    public  toSvg(){
+
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+    public Point getP2() {
+        return p2;
+    }
+
 }
