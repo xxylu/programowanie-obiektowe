67cd94a341206012a8082ec1c2e9d2291a6bff39
xxylu <80449534+xxylu@users.noreply.github.com>
2024-03-15 12:21:47 +0100

    poprawki

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Point p1 = new Point(120,40);
         Point p2 = new Point(10,76);
+        Point p3 = new Point(10,76);
 
         Segment s = new Segment(p1,p2);
         System.out.println(s.distance());
